f72ddf5b524c59a9ed747414c369d1666a93d37f
kaito <kaitoseto1129@gmail.com>
2026-07-29 11:35:26 +0900

    Initial Commit

---
+++ Matching App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Matching App
+//
+//  Created by Kaito Seto on 2026/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Matching App/Matching_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matching_AppApp.swift
+//  Matching App
+//
+//  Created by Kaito Seto on 2026/07/29.
+//
+
+import SwiftUI
+
+@main
+struct Matching_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
